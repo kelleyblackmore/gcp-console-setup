00832c052bc82dc7fa832d863777a719e1bef6dc
Kris Kelley <kelleyblackmore@gmail.com>
2019-04-01 13:31:06 -0600

    Update start_up.sh

--- start_up.sh
+++ start_up.sh
@@ -6,8 +6,10 @@ dpkg -i inspec_3.7.11-1_amd64.deb
 inspec --version
 
 
-curl -sSL https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip -o nomad.zip
-unzip nomad.zip
+wget https://releases.hashicorp.com/nomad/0.8.7/nomad_0.8.7_linux_amd64-lxc.zip
+unzip nomad_0.8.7_linux_amd64-lxc.zip
 sudo install nomad /usr/bin/nomad
 sudo mkdir -p /etc/nomad.d
 sudo chmod a+w /etc/nomad.d
+echo "Installing autocomplete..."
+nomad -autocomplete-install
